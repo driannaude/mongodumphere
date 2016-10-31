b2d14e46bb77f01f34d66d762c5f7287781f5bb7
Drian Naude <drian.naude@truescape.com>
2016-10-31 14:57:15 +1300

    Added ssh tunneling

--- mongodumphere.js
+++ mongodumphere.js
@@ -14,25 +14,96 @@ script
   .option('-p, --password [password]', 'Specify password for SSH tunnel')
   .option('-P, --port [port]', 'Specify port for SSH tunnel', 22)
   .parse(process.argv);
+
+
+// Pads date elements < 10 with a leading zero
+function padEarly(num) {
+  return num < 10 ? '0' + num : num.toString();
+}
+
+var date = new Date();
+var day = padEarly(date.getDate());
+var month = padEarly(date.getMonth() + 1);
+var year = date.getFullYear();
+var todaysDate = day + '-' + month + '-' + year;
+var remoteFolder = '~/mongo-backups/' + todaysDate;
+console.log(remoteFolder);
+
 var shouldUseTunneling = true;
 if (!script.host) {
   console.log(colors.yellow('\n[!] No hostname specified, using localhost and no tunnel\n'));
   shouldUseTunneling = false;
 }
 if (!shouldUseTunneling) {
-  child_process.execFile('mongodump', ['--gzip', '--archive=./mongodump.gz'], function(error, stdout, stderr) {
-    if(error){
+  child_process.exec('mongodump', ['--gzip', '--archive=./mongodump.gz'], function (error, stdout, stderr) {
+    if (error) {
       throw error;
-      process.exit(1);
     }
     console.log(stdout);
     console.log(stderr);
   });
 }
 if (shouldUseTunneling) {
-  if (script.username || !script.password || script.port) {
+  sshTunnelingProcedure();
+}
+
+function createLocalFoldersIfNotExist(callback){
+  child_process.exec('mkdir -p ' + remoteFolder, [], function (error, stdout, stderr) {
+    if (error) {
+      
+      console.log('[1]', stdout);
+      console.log('[2]',stderr);
+      throw error;
+    }
+    console.log('[-1]', stdout);
+    console.log('[0]',stderr);
+    if(callback && typeof callback === 'function'){
+      callback();
+    }  
+  });
+  
+}
+
+function copyMongoDumpToLocalMachine(){
+  child_process.exec('scp drian@' + script.host + ':'+remoteFolder+'/dump-' + todaysDate + '.gz '+remoteFolder + '/dump-'+todaysDate + '.gz', [], function (error, stdout, stderr) {
+    if (error) {
+      console.log('[5]', stdout);
+      console.log('[6]',stderr);
+      throw error;
+    }
+    console.log('[3]', stdout);
+      console.log('[4]',stderr);
+  });
+}
+
+// handler for stdout or error when for remote ssh tunnel execution
+function remoteExecHandler(error, stdout, stderr) {
+  if (error) {
+    console.log(colors.red('[  ERROR]: An error has occured'));
+    console.error(stderr);
+    throw error;
+  }
+  console.log(stdout);
+  createLocalFoldersIfNotExist(copyMongoDumpToLocalMachine());
+}
+
+function sshTunnelingProcedure() {
+  if (!script.username || !script.password) {
     console.log(colors.red('\n[!] No username or password specified.\n'));
     script.outputHelp();
+    process.exit(1);
   }
-  console.log('Reverse Tunneling into: ' + script.host);
+  if (!script.port) {
+    console.log(colors.red('\n[!] No port specified, using default [22].\n'));
+    script.port = 22;
+  }
+
+  console.log(colors.green('[ INFO]: Reverse Tunneling into [' + script.host + '] using port [' + script.port + '] as user [' + script.username + ']'));
+  // Eventually we will go a gzip upgrade to mongo 3.2
+  //'mkdir -p ' + remoteFolder + ' && cd ' + remoteFolder + ' && mongodump --gzip --archive=./dump-' + todaysDate + '.gz'
+  exec('mkdir -p ' + remoteFolder + ' && cd ' + remoteFolder + ' && mongodump --gzip --archive=./dump-' + todaysDate + '.gz', {
+    user: script.username,
+    host: script.host,
+    password: script.password
+  }, remoteExecHandler);
 }
